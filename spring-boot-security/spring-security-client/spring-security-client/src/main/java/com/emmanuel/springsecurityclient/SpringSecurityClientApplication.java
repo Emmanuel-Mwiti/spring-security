faa2172f5aee0f8678f04e7dc303d3608121c0ad
Emmanuel Mwiti <emmanuel.mwiti@turnkeyafrica.com>
2024-01-13 18:13:14 +0300

    -This is the first commit for the spring boot project

---
+++ spring-boot-security/spring-security-client/spring-security-client/src/main/java/com/emmanuel/springsecurityclient/SpringSecurityClientApplication.java
@@ -0,0 +1,13 @@
+package com.emmanuel.springsecurityclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityClientApplication.class, args);
+	}
+
+}
